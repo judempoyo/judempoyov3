011b662980d154ae60d7926882c125fbcd2efe3d
Jude mpoyo <mpoyojude0@gmail.com>
2026-01-01 20:15:59 +0200

    feat : create toggletheme component and update css main file

---
+++ app/components/ToggleTheme.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+onMounted(() => {
+    if (!colorMode.preference) colorMode.preference = 'system'
+})
+
+const toggleTheme = () => {
+  colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+}
+</script>
+
+<template>
+  <button
+    @click="toggleTheme"
+    class="group relative flex items-center bg-zinc-200 dark:bg-zinc-800/60 p-1 rounded-md w-20 h-8 transition-all duration-300 overflow-hidden select-none"
+    :title="colorMode.value === 'dark' ? 'Switch to Light Mode' : 'Switch to Dark Mode'"
+  >
+
+    <!-- Curseur animé -->
+    <div
+      class="absolute inset-y-1 w-[38px] bg-primary transition-all duration-500 ease-[cubic-bezier(0.23,1,0.32,1)] rounded-md flex items-center justify-center shadow-md dark:shadow-sm z-10"
+      :class="colorMode.value === 'dark' ? 'left-1' : 'left-[calc(100%-42px)]'"
+    >
+      <div class="relative w-4 h-4 text-primary-foreground ">
+
+        <!-- Icône Moon -->
+        <svg
+          viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3"
+          class="absolute inset-0 transition-all duration-500"
+          :class="colorMode.value === 'dark' ? 'scale-100 rotate-0 opacity-100' : 'scale-0 rotate-90 opacity-0'"
+        >
+          <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+        </svg>
+
+        <!-- Icône Sun -->
+        <svg
+          viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3"
+          class="absolute inset-0 transition-all duration-500"
+          :class="colorMode.value === 'light' ? 'scale-100 rotate-0 opacity-100' : 'scale-0 -rotate-90 opacity-0'"
+        >
+          <circle cx="12" cy="12" r="4" />
+          <path d="M12 2v2M12 20v2M2 12h2M20 12h2" />
+          <path d="m4.93 4.93 1.41 1.41m11.32 11.32 1.41 1.41m-1.41-11.32 1.41-1.41m-11.32 11.32-1.41 1.41" />
+        </svg>
+
+      </div>
+    </div>
+
+    <!-- Labels -->
+    <div class="flex justify-between w-full px-2.5 font-black text-[8px] tracking-tighter">
+      <span
+        class="transition-all duration-500"
+        :class="colorMode.value === 'dark' ? 'text-zinc-100 opacity-0' : 'text-zinc-600 opacity-100'"
+      >DARK</span>
+
+      <span
+        class="transition-all duration-500"
+        :class="colorMode.value === 'light' ? 'text-zinc-100 opacity-100' : 'text-zinc-900 opacity-100'"
+      >LIGHT</span>
+    </div>
+
+  </button>
+</template>
